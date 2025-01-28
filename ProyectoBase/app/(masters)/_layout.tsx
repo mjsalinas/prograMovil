9552f415b9b96b783cae31ff9653793341c7309a
Maria Salinas <maria.salinas@kopisusa.com>
2025-01-27 20:28:26 -0600

    agregando MastersLayout para navegacion en pilas

---
+++ ProyectoBase/app/(masters)/_layout.tsx
@@ -0,0 +1,24 @@
+import { useColorScheme } from "@/hooks/useColorScheme";
+import { DarkTheme, DefaultTheme, ThemeProvider } from "@react-navigation/native";
+import { Stack } from "expo-router";
+import { StatusBar } from "expo-status-bar";
+
+export default function MastersLayout() {
+
+    const colorScheme = useColorScheme();
+
+    return (
+        <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+            <Stack
+                screenOptions={{
+                    headerShown: true,
+                }}
+            >
+                <Stack.Screen name="registration" options={{ title: "Registro" }} />
+            </Stack>
+            <StatusBar style="auto" />
+
+        </ThemeProvider>
+
+    );
+}
