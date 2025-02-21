f7c9d1f03fac897ca0232b8eaa07c7eefdf1d8b7
HDVLAP-SOPORTE2\IT HDV <josberhnz@unitec.edu>
2025-02-20 22:11:56 -0600

    *se añade name y profilePic en User *uso de require para la imagen de perfil

--- ProyectoBase/contexts/AuthContext.tsx
+++ ProyectoBase/contexts/AuthContext.tsx
@@ -1,9 +1,13 @@
-import { createContext, isValidElement, useContext, useState } from "react"
+import { createContext, useContext, useState } from "react";
 
-type User = {email: string} | null;
+type User = { 
+    email: string;
+    name: string;
+    profilePic: string;
+} | null;
 
 const AuthContext = createContext<{
-    user: User,
+    user: User;
     isAllowed: boolean;
     login: (email: string) => void;
     logout: () => void;
@@ -11,36 +15,39 @@ const AuthContext = createContext<{
 
 export const useAuth = () => {
     const context = useContext(AuthContext);
-    if (!context) throw new Error ("useAuth debe usarse dentro de AuthProvider");
+    if (!context) throw new Error("useAuth debe usarse dentro de AuthProvider");
     return context;
 }
 
-export const AuthProvider = ({children}: {children: React.ReactNode}) => {
+export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
     const [user, setUser] = useState<User>(null);
     const [isAllowed, setIsAllowed] = useState<boolean>(false);
 
-    const login =  (email: string) => {
+    const login = (email: string) => {
         const isValidEmail = email.endsWith('.edu');
 
-        if (isValidEmail){
-            setUser({email});
+        if (isValidEmail) {
+            setUser({
+                email,
+                name: "Juan Pérez",
+                profilePic: require("../assets/images/profilepic.jpg")
+            });
             setIsAllowed(true);
-        }else{
+        } else {
             setUser(null);
             setIsAllowed(false);
-            alert("Solo correos .edu pueden ingresar")
+            alert("Solo correos .edu pueden ingresar");
         }
     };
 
     const logout = () => {
         setUser(null);
         setIsAllowed(false);
-    }
+    };
 
     return (
-        <AuthContext.Provider value={{user, isAllowed, login, logout}}>
+        <AuthContext.Provider value={{ user, isAllowed, login, logout }}>
             {children}
         </AuthContext.Provider>
-    )
-   
-}
+    );
+};
