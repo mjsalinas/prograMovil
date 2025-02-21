4bc913e9fc0b8edeea22712baf9d47b811fc8aed
HDVLAP-SOPORTE2\IT HDV <josberhnz@unitec.edu>
2025-02-20 22:13:46 -0600

    *Se maneja el nombre del usuario desde el contexto *Se agrega la imagen de perfil *Se mejora la interfaz con StyleSheet

--- ProyectoBase/app/tabs/profile.tsx
+++ ProyectoBase/app/tabs/profile.tsx
@@ -1,23 +1,63 @@
 import { useAuth } from "@/contexts/AuthContext";
 import { useFocusEffect } from "expo-router";
 import { useCallback, useState } from "react";
-import { View, Text, TextInput } from "react-native";
+import { View, Text, TextInput, Image, StyleSheet } from "react-native";
 
 export default function ProfileScreen() {
     const { user } = useAuth();
-    const [nombre, setNombre] = useState('');
+    const [nombre, setNombre] = useState(user?.name || "");
 
-// codigo para limpiar estado en cambio de tabs
-    // useFocusEffect(
-    //     useCallback(() => {
-    //         return () => setNombre('');
-    //     }, [])
-    // );
+    useFocusEffect(
+        useCallback(() => {
+            return () => setNombre(user?.name || '');
+        }, [user])
+    );
 
     return (
-        <View>
-            <Text style={{ fontSize: 24, fontWeight: 'bold' }}>Perfil de {nombre}</Text>
-            <TextInput placeholder="Nombre" value={nombre} onChangeText={setNombre} />
+        <View style={styles.container}>
+            {user?.profilePic && (
+                <Image source={{ uri: user.profilePic }} style={styles.profileImage} />
+            )}
+
+            <Text style={styles.title}>Perfil de {nombre}</Text>
+            
+            <TextInput
+                style={styles.input}
+                placeholder="Nombre"
+                value={nombre}
+                onChangeText={setNombre}
+            />
         </View>
     );
 }
+
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: "center",
+        justifyContent: "center",
+        backgroundColor: "#f4f4f4",
+        padding: 20,
+    },
+    profileImage: {
+        width: 100,
+        height: 100,
+        borderRadius: 50,
+        marginBottom: 20,
+    },
+    title: {
+        fontSize: 24,
+        fontWeight: "bold",
+        marginBottom: 10,
+    },
+    input: {
+        width: "80%",
+        padding: 10,
+        borderWidth: 1,
+        borderColor: "#ccc",
+        borderRadius: 8,
+        backgroundColor: "#fff",
+    },
+});
+
