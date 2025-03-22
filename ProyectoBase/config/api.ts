946a2a6380a25b283905367c8122233d4549b2c5
Maria Jose Salinas <mariasalinas713@gmail.com>
2025-03-21 19:09:20 -0600

    archivo para manejar URL base del origen de solicitudes al servidor

---
+++ ProyectoBase/config/api.ts
@@ -0,0 +1,15 @@
+import { Platform } from "react-native";
+//para web o utilizar en la computadora
+const LOCALHOST = "localhost:5144";
+
+//para emulador android
+const EMULATOR_HOST = "10.0.2.2:5144";
+
+//dispositivo fisico (expo go) utilizando la ip de la computadora
+const DEVICE_HOST = "192.168.1.212";
+
+export const BASE_URL =
+  Platform.OS === "android"
+    ? (__DEV__ ? `http://${EMULATOR_HOST}` : `http://${DEVICE_HOST}`)
+    : `http://${LOCALHOST}`;
+
