e99472e21b447d91f81aa7fff8296994fd1f3a8d
Maria Jose Salinas <mariasalinas713@gmail.com>
2025-03-23 13:22:02 -0600

    agregando flatlist para mostrar usuarios alojados en el estado

--- ProyectoBase/app/tabs/settings.tsx
+++ ProyectoBase/app/tabs/settings.tsx
@@ -1,31 +1,33 @@
-import { View, Text, Button, TouchableOpacity } from "react-native";
+import { View, Text, Button, TouchableOpacity, FlatList } from "react-native";
 import { useRouter } from "expo-router";
 import { useAuth } from "@/contexts/AuthContext";
 import { useTheme } from "@/contexts/ThemeContext";
 import { darkTheme, lightTheme } from "@/styles/themes";
 import { useEffect } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import axios from "axios";
 import { setUsers } from "@/store/slices/usersSlice";
 import { BASE_URL } from "@/config/api";
+import { RootState } from "@/store/store";
 
 export default function SettingsScreen() {
     const { theme, toggleTheme } = useTheme();
     const styles = theme === "dark" ? darkTheme : lightTheme;
     const dispatch = useDispatch();
+    const users = useSelector((state: RootState) => state.users);
 
-    useEffect(()=>{
+    useEffect(() => {
         const fetchUsers = async () => {
             try {
                 const response = await axios.get(BASE_URL + "/api/usuarios");
                 dispatch(setUsers(response.data))
             }
-            catch(err: any) {
+            catch (err: any) {
                 console.log("Error de Axios: ", err.message);
                 console.log("Error completo: ", err.toJSON?.());
             }
         };
-    fetchUsers();
+        fetchUsers();
     }, [dispatch])
     return (
         <View style={styles.container}>
@@ -40,6 +42,22 @@ export default function SettingsScreen() {
                     style={styles.buttonText}
                 >Cambiar Tema</Text>
             </TouchableOpacity>
+
+            <View>
+                <Text>Usuarios Registrados</Text>
+                <FlatList
+                    data={users}
+                    keyExtractor={(item) => item.id.toString()}
+                    renderItem={
+                        ({ item }) => (
+                            <View>
+                                <Text>{item.nombre}</Text>
+                                <Text>{item.correo}</Text>
+                            </View>)
+                    }
+                />
+            </View>
         </View>
     )
 }
+
